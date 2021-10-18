be6866b802f4236e7c3d487917a86f4f8ae1c106
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-10-18 23:26:41 +0900

    作業保存

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,17 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { FirebaseOptions, initializeApp } from 'firebase/app';
+import { Auth, getAuth, signInWithEmailAndPassword, UserCredential } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// const signInArgs: [Auth, string, string] = [auth, 'a', 'a'];
+const { auth,email,password } = {auth:auth,email:'',password:''};
+const signIn = signInWithEmailAndPassword(signInArgs.auth,);

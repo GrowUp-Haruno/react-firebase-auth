eb0f01ba9996c4604c0d3a377f0082e28b03ea09
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-10-19 23:28:19 +0900

    作業保存

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,9 +1,72 @@
-import { FC } from 'react';
+import { createUserWithEmailAndPassword } from '@firebase/auth';
+import { ChangeEventHandler, FC, FormEventHandler, useState } from 'react';
+import { auth } from '../firebase';
 
 //Propsの型定義
 type PropsType = {};
 
-const Signup: FC<PropsType> = () => <h1>ユーザー登録</h1>;
+const Signup: FC<PropsType> = () => {
+  type initilaUserTypes = {
+    email: string;
+    password: string;
+  };
+  const initialSignupUser: initilaUserTypes = { email: '', password: '' };
+  const [signupUser, setSignupUser] = useState<initilaUserTypes>(initialSignupUser);
+
+  const [isDesable, setIsDesable] = useState(false);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSignupUser({ ...signupUser, [`${event.target.id}`]: event.target.value });
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    const { email, password } = signupUser;
+    event.preventDefault();
+    setIsDesable(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((UserCredential) => {
+        console.log(UserCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsDesable(false);
+        setSignupUser(initialSignupUser);
+      });
+  };
+  return (
+    <>
+      <h1>ユーザー登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={handleChange}
+            value={signupUser.email}
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={handleChange}
+            value={signupUser.password}
+          />
+        </div>
+        <div>
+          <button disabled={isDesable}>登録</button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 Signup.displayName = 'Signup';
 export default Signup;
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { FirebaseOptions, initializeApp } from 'firebase/app';
-import { Auth, getAuth, signInWithEmailAndPassword, UserCredential } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -17,8 +12,4 @@ const firebaseConfig: FirebaseOptions = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// const signInArgs: [Auth, string, string] = [auth, 'a', 'a'];
-const { auth,email,password } = {auth:auth,email:'',password:''};
-const signIn = signInWithEmailAndPassword(signInArgs.auth,);
+export const auth = getAuth(app);

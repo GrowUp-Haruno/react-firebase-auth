3f45321ef6fc490bdf3aaf795a701941091d0a3e
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-10-24 23:15:21 +0900

    ロジック分離

---
+++ src/components/hooks/useHandleSubmitFirebaseCreateUse.tsx
@@ -0,0 +1,35 @@
+import { FirebaseError } from '@firebase/util';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { FormEventHandler, useCallback } from 'react';
+import { auth } from '../../firebase';
+import { AcountUserTypes } from '../types/typeSignup';
+
+export const useHandleSubmitFirebaseCreateUser = (
+  initialSignupUser: AcountUserTypes,
+  signupUser: AcountUserTypes,
+  setSignupUser: React.Dispatch<React.SetStateAction<AcountUserTypes>>,
+  setIsButtonDesable: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  const handleSubmitFirebaseCreateUser = useCallback<FormEventHandler<HTMLFormElement>>(
+    async (event) => {
+      const { email, password } = signupUser;
+      event.preventDefault();
+      setIsButtonDesable(true);
+      try {
+        const UserCredential = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(UserCredential)
+      } catch (error) {
+        if (error instanceof FirebaseError) {
+          console.log(error.code);
+        } else {
+          console.log(error);
+        }
+      } finally {
+        setIsButtonDesable(false);
+        setSignupUser(initialSignupUser);
+      }
+    },
+    [initialSignupUser, setIsButtonDesable, setSignupUser, signupUser]
+  );
+  return { handleSubmitFirebaseCreateUser };
+};

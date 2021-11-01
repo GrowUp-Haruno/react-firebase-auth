989379cd44b1165fdb8f4b1bb0b71df69dfe4105
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-01 14:12:33 +0900

    ユーザー情報のセット手順を変更

--- src/pages/hooks/useApp.tsx
+++ src/pages/hooks/useApp.tsx
@@ -1,4 +1,4 @@
-import { onAuthStateChanged, User } from 'firebase/auth';
+import { onAuthStateChanged, signOut, User } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 
 import { auth } from '../../firebase';
@@ -7,9 +7,20 @@ import { useAppTypes } from '../types/typeApp';
 export const useApp: useAppTypes = () => {
   const [signInUser, setSignInUser] = useState<User | null>(null);
   useEffect(() => {
+    // ユーザーのサインイン状態の変更に対するオブザーバーを追加
     onAuthStateChanged(auth, (user: User | null) => {
       if (user) {
-        setSignInUser(user);
+        // メールアドレスの認証(user.emailVerified)が取れているか確認
+        if (user.emailVerified) {
+          // ユーザー情報をセット
+          setSignInUser(user);
+        } else {
+          // サインアウト
+          (async () => {
+            await signOut(auth);
+            console.log('メールアドレスの認証が取れていないためサインアウトしました');
+          })();
+        }
       } else {
         console.log('user Sign out');
       }

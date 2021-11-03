b7bf7cd797c0ca124779eadfbea26511b83118ed
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-03 21:30:01 +0900

    カスタムフックを汎用的に改修

--- src/components/hooks/useFirebase.tsx
+++ src/components/hooks/useFirebase.tsx
@@ -3,18 +3,22 @@ import {
   sendEmailVerification,
   signInWithEmailAndPassword,
   signOut,
+  updateCurrentUser,
+  updateProfile,
 } from 'firebase/auth';
 import { useCallback } from 'react';
 
 import { auth } from '../../firebase';
-import { useFirebaseTypes } from '../types/typeUseFirebase';
+// import { AcountUserTypes } from '../types/typeSign';
+
+import { changeUserProfileTypes, signTypes, useFirebaseTypes } from '../types/typeUseFirebase';
 
 export const useFirebase: useFirebaseTypes = () => {
   /** ユーザー登録 */
-  const signUp = useCallback(async (email: string, password: string) => {
+  const signUp: signTypes = useCallback(async (arg) => {
     // 入力フに送信して、ユーザーを登録する
     // 成功した場合、ログイン状態となりユーザー情報を取得する
-    const UserCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const UserCredential = await createUserWithEmailAndPassword(auth, arg.email, arg.password);
 
     // Firebase AuthenticationのcreateUserWithEmailAndPassword()はメールアドレスの被りや
     // パスワードが不適切などの理由を除いていくらでもユーザー登録ができて、ログインもできてしまう仕様のため、
@@ -28,8 +32,8 @@ export const useFirebase: useFirebaseTypes = () => {
   }, []);
 
   /** サインイン */
-  const signIn = useCallback(async (email: string, password: string) => {
-    await signInWithEmailAndPassword(auth, email, password);
+  const signIn: signTypes = useCallback(async (arg) => {
+    await signInWithEmailAndPassword(auth, arg.email, arg.password);
   }, []);
 
   /** サインアウト */
@@ -37,5 +41,16 @@ export const useFirebase: useFirebaseTypes = () => {
     await signOut(auth);
   }, []);
 
-  return { signUp, signIn, userSignOut };
+  /** ユーザープロファイル更新 */
+  const changeUserProfile:changeUserProfileTypes = useCallback(async () => {
+    if (auth.currentUser !== null) {
+      await updateProfile(auth.currentUser, {
+        displayName: `${auth.currentUser.displayName}a`,
+        photoURL: 'test',
+      });
+      await updateCurrentUser(auth, auth.currentUser);
+    }
+  }, []);
+
+  return { signUp, signIn, userSignOut, changeUserProfile };
 };

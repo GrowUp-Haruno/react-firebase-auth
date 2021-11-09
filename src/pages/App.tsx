264af7914f0585c6ad1f1b00cebf4e863772b55c
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-09 13:02:08 +0900

    マウントするコンポーネントを追加

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,13 +3,21 @@ import './App.css';
 import { SignUp } from '../components/SignUp';
 import { useApp } from './hooks/useApp';
 import { SignOut } from '../components/SignOut';
+import { ChangeProfile } from '../components/ChangeProfile';
 
 const App = () => {
   const { signInUser } = useApp();
   return (
     <div className="App">
       <SignUp />
-      {signInUser === null ? <SignIn /> : <SignOut />}
+      {signInUser === null ? (
+        <SignIn />
+      ) : (
+        <>
+          <ChangeProfile />
+          <SignOut />
+        </>
+      )}
     </div>
   );
 };

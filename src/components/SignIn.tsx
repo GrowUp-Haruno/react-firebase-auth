b57546dbbe6f110bdd6d837636a48c287e59b46d
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-10 23:29:54 +0900

    Cardコンポーネント適用

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,33 +2,35 @@ import { FC, memo } from 'react';
 import { useSignIn } from './hooks/useSignIn';
 import { AcountUserTypes } from './types/typeSign';
 import { FormInterfase } from '../interface/FormInterfase';
+import Card from '../atoms/Card';
 
-export const SignIn:FC = memo(() => {
-  const { inputValueState, buttonState, handleChangeObjectState, handleSubmit } =
-    useSignIn();
+export const SignIn: FC = memo(() => {
+  const { inputValueState, buttonState, handleChangeObjectState, handleSubmit } = useSignIn();
   return (
-    <FormInterfase<AcountUserTypes>
-      formTitle="ログイン"
-      inputParts={[
-        {
-          labelName: 'メールアドレス',
-          inputName: 'email',
-          inputType: 'email',
-          inputPlaceholder: 'メールアドレス',
-        },
-        {
-          labelName: 'パスワード',
-          inputName: 'password',
-          inputType: 'password',
-          inputPlaceholder: 'パスワード',
-        },
-      ]}
-      handleSubmit={handleSubmit}
-      handleChange={handleChangeObjectState}
-      inputValueState={inputValueState}
-      buttonName="ログイン"
-      buttonState={buttonState}
-    />
+    <Card>
+      <FormInterfase<AcountUserTypes>
+        formTitle="ログイン"
+        inputParts={[
+          {
+            labelName: 'メールアドレス',
+            inputName: 'email',
+            inputType: 'email',
+            inputPlaceholder: 'メールアドレス',
+          },
+          {
+            labelName: 'パスワード',
+            inputName: 'password',
+            inputType: 'password',
+            inputPlaceholder: 'パスワード',
+          },
+        ]}
+        handleSubmit={handleSubmit}
+        handleChange={handleChangeObjectState}
+        inputValueState={inputValueState}
+        buttonName="ログイン"
+        buttonState={buttonState}
+      />
+    </Card>
   );
 });
 

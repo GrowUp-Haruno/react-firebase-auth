7e48bdb6666bd4bec68d8958bd1c21ea5fca476d
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-10 22:12:09 +0900

    storybookをインストール

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -4,7 +4,6 @@ import { useFirebase } from './hooks/useFirebase';
 export const SignOut: FC = memo(() => {
   return (
     <>
-      <h1>サインアウト</h1>
       <button onClick={useFirebase().userSignOut}>サインアウト</button>
     </>
   );

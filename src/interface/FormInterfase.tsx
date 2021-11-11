7994cc3d15b0a6781bd5dcf272227f450a98d768
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-11 16:34:52 +0900

    パスワードの表示切替機能を追加（バラック実装）

--- src/interface/FormInterfase.tsx
+++ src/interface/FormInterfase.tsx
@@ -1,6 +1,4 @@
-// User import
-import { FormInterfasePropTypes, FormInputValueTypes } from './types/typeFormInterfase';
-
+import { useState } from 'react';
 import {
   FormControl,
   FormLabel,
@@ -10,7 +8,12 @@ import {
   Heading,
   Box,
   Button,
+  InputRightElement,
+  InputGroup,
 } from '@chakra-ui/react';
+
+// User import
+import { FormInterfasePropTypes, FormInputValueTypes } from './types/typeFormInterfase';
 
 /**
  * # FormInterfase
@@ -31,43 +34,68 @@ export const FormInterfase = <T extends FormInputValueTypes>({
   buttonState,
   buttonName,
 }: FormInterfasePropTypes<T>): JSX.Element => {
+  const [isShow, setIsShow] = useState(false);
   return (
     <Stack spacing={8} mx={'auto'} maxW={'lg'} py={4} px={6}>
       <Box align={'flex-start'}>
         <Heading fontSize="3xl">{formTitle}</Heading>
       </Box>
-        <Stack spacing={16} as="form" onSubmit={handleSubmit}>
-          <Stack spacing={4}>
-            {inputParts.map(
-              ({ labelName, nowSetting, inputName, inputType, inputPlaceholder }, index) => {
-                return (
-                  <FormControl>
-                    <FormLabel>{labelName}</FormLabel>
-                    <Input
-                      name={inputName}
-                      type={inputType}
-                      placeholder={inputPlaceholder}
-                      onChange={handleChange}
-                      value={inputValueState[`${inputName}`]}
-                      key={`${inputName}-${index}`}
-                    />
-                    {typeof nowSetting !== 'undefined' && (
-                      <FormHelperText>現在の設定: {nowSetting}</FormHelperText>
+      <Stack spacing={16} as="form" onSubmit={handleSubmit}>
+        <Stack spacing={4}>
+          {inputParts.map(
+            ({ labelName, nowSetting, inputName, inputType, inputPlaceholder }, index) => {
+              return (
+                <FormControl>
+                  <FormLabel>{labelName}</FormLabel>
+                  <InputGroup>
+                    {inputType === 'password' ? (
+                      // パスワードの時のみ表示
+                      <>
+                        <Input
+                          name={inputName}
+                          type={isShow ? 'text' : inputType}
+                          placeholder={inputPlaceholder}
+                          onChange={handleChange}
+                          value={inputValueState[`${inputName}`]}
+                          key={`${inputName}-${index}`}
+                          pr={'4.5rem'}
+                        />
+                        <InputRightElement w="4.5rem">
+                          <Button h="1.75rem" size="sm" onClick={() => setIsShow(!isShow)}>
+                            {isShow ? '隠す' : '表示'}
+                          </Button>
+                        </InputRightElement>
+                      </>
+                    ) : (
+                      // 通常表示
+                      <Input
+                        name={inputName}
+                        type={inputType}
+                        placeholder={inputPlaceholder}
+                        onChange={handleChange}
+                        value={inputValueState[`${inputName}`]}
+                        key={`${inputName}-${index}`}
+                        pr={'4.5rem'}
+                      />
                     )}
-                  </FormControl>
-                );
-              }
-            )}
-          </Stack>
-          <Button
-            disabled={buttonState}
-            backgroundColor={'blue.300'}
-            color={'gray.100'}
-            type="submit"
-          >
-            {buttonName}
-          </Button>
+                  </InputGroup>
+                  {typeof nowSetting !== 'undefined' && (
+                    <FormHelperText>現在の設定: {nowSetting}</FormHelperText>
+                  )}
+                </FormControl>
+              );
+            }
+          )}
         </Stack>
+        <Button
+          disabled={buttonState}
+          backgroundColor={'blue.300'}
+          color={'gray.100'}
+          type="submit"
+        >
+          {buttonName}
+        </Button>
+      </Stack>
       {/* </form> */}
     </Stack>
   );

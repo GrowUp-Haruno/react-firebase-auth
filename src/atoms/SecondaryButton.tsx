0b0802d8460723870b987f5c0e3bcd1ce079feda
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-14 17:56:01 +0900

    prop修正

--- src/atoms/SecondaryButton.tsx
+++ src/atoms/SecondaryButton.tsx
@@ -7,21 +7,19 @@ type PropsType = {
   buttonName: string;
   type?: 'button' | 'submit' | 'reset';
   children?: ReactNode;
-  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-const SecondaryButton: FC<PropsType> = ({ buttonState, buttonName, type,children,handleClick }) => {
+const SecondaryButton: FC<PropsType> = ({ buttonState, buttonName, type, children, onClick }) => {
   return (
     <Button
-      isLoading={buttonState}
-      loadingText={`${buttonName}中です`}
       disabled={buttonState}
       backgroundColor={'green.300'}
       color={'gray.100'}
       type={type}
-      _hover={{ backgroundColor: 'greeb.500' }}
+      _hover={{ backgroundColor: 'green.500' }}
       _loading={{ backgroundColor: 'gray.500' }}
-      onClick={handleClick}
+      onClick={onClick}
     >
       {children || buttonName}
     </Button>

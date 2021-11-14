31f02a6d818369e5c3041b9517fe877ba84acca4
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-14 18:12:11 +0900

    コンポーネント切替ボタンを追加

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -6,47 +6,52 @@ import Card from '../atoms/Card';
 import { Stack, Heading, Spacer } from '@chakra-ui/react';
 import SecondaryButton from '../atoms/SecondaryButton';
 
-export const SignIn: FC<{ setIsSignIn: Dispatch<SetStateAction<boolean>> }> = memo(
-  ({ setIsSignIn }) => {
-    const { inputValueState, buttonState, handleChangeObjectState, handleSubmit } = useSignIn();
-    return (
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Heading fontSize="3xl">🎉チャットアプリへようこそ🎉</Heading>
-        <Card>
-          <FormInterfase<AcountUserTypes>
-            formTitle="ログイン"
-            inputParts={[
-              {
-                labelName: 'メールアドレス',
-                inputName: 'email',
-                inputType: 'email',
-                inputPlaceholder: 'メールアドレス',
-              },
-              {
-                labelName: 'パスワード',
-                inputName: 'password',
-                inputType: 'password',
-                inputPlaceholder: 'パスワード',
-              },
-            ]}
-            handleSubmit={handleSubmit}
-            handleChange={handleChangeObjectState}
-            inputValueState={inputValueState}
-            buttonName="ログイン"
-            buttonState={buttonState}
-          />
-          <Spacer />
-          <SecondaryButton buttonName="新規登録" type="button" buttonState={buttonState} />
-          <SecondaryButton
-            buttonName="パスワードを忘れた方はこちら"
-            type="button"
-            buttonState={buttonState}
-          />
- 
-        </Card>
-      </Stack>
-    );
-  }
-);
+type PropsType = { setMode: Dispatch<SetStateAction<'SignIn' | 'SignUp' | 'ResetPassword'>> };
+
+export const SignIn: FC<PropsType> = memo(({ setMode }) => {
+  const { inputValueState, buttonState, handleChangeObjectState, handleSubmit } = useSignIn();
+  return (
+    <Stack spacing={8} mx={'auto'} maxW={'xl'} minW={'lg'} py={12} px={6}>
+      <Heading fontSize="3xl">🎉チャットアプリへようこそ🎉</Heading>
+      <Card>
+        <FormInterfase<AcountUserTypes>
+          formTitle="ログイン"
+          inputParts={[
+            {
+              labelName: 'メールアドレス',
+              inputName: 'email',
+              inputType: 'email',
+              inputPlaceholder: 'メールアドレス',
+            },
+            {
+              labelName: 'パスワード',
+              inputName: 'password',
+              inputType: 'password',
+              inputPlaceholder: 'パスワード',
+            },
+          ]}
+          handleSubmit={handleSubmit}
+          handleChange={handleChangeObjectState}
+          inputValueState={inputValueState}
+          buttonName="ログイン"
+          buttonState={buttonState}
+        />
+        <Spacer />
+        <SecondaryButton
+          buttonName="新規登録はこちら"
+          type="button"
+          buttonState={buttonState}
+          onClick={() => setMode('SignUp')}
+        />
+        <SecondaryButton
+          buttonName="パスワードを忘れた方はこちら"
+          type="button"
+          buttonState={buttonState}
+          onClick={() => setMode('ResetPassword')}
+        />
+      </Card>
+    </Stack>
+  );
+});
 
 SignIn.displayName = 'SignIn';
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,37 +1,57 @@
-import { FC, memo } from 'react';
+import { Heading, Spacer, Stack } from '@chakra-ui/react';
+import { Dispatch, FC, memo, SetStateAction } from 'react';
 import Card from '../atoms/Card';
+import SecondaryButton from '../atoms/SecondaryButton';
 import { FormInterfase } from '../interface/FormInterfase';
 
 import { useSignUp } from './hooks/useSignUp';
 import { AcountUserTypes } from './types/typeSign';
 
-export const SignUp: FC = memo(() => {
+type PropsType = { setMode: Dispatch<SetStateAction<'SignIn' | 'SignUp' | 'ResetPassword'>> };
+
+export const SignUp: FC<PropsType> = memo(({ setMode }) => {
   const { inputValueState, buttonState, handleChangeObjectState, handleSubmit } = useSignUp();
   return (
-    <Card>
-      <FormInterfase<AcountUserTypes>
-        formTitle="ユーザー登録"
-        inputParts={[
-          {
-            labelName: 'メールアドレス',
-            inputName: 'email',
-            inputType: 'email',
-            inputPlaceholder: 'メールアドレス',
-          },
-          {
-            labelName: 'パスワード',
-            inputName: 'password',
-            inputType: 'password',
-            inputPlaceholder: 'パスワード',
-          },
-        ]}
-        handleSubmit={handleSubmit}
-        handleChange={handleChangeObjectState}
-        inputValueState={inputValueState}
-        buttonName="登録"
-        buttonState={buttonState}
-      />
-    </Card>
+    <Stack spacing={8} mx={'auto'} maxW={'xl'} minW={'lg'} py={12} px={6}>
+      <Heading fontSize="3xl">ユーザー登録</Heading>
+      <Card>
+        <FormInterfase<AcountUserTypes>
+
+          inputParts={[
+            {
+              labelName: 'メールアドレス',
+              inputName: 'email',
+              inputType: 'email',
+              inputPlaceholder: 'メールアドレス',
+            },
+            {
+              labelName: 'パスワード',
+              inputName: 'password',
+              inputType: 'password',
+              inputPlaceholder: 'パスワード',
+            },
+          ]}
+          handleSubmit={handleSubmit}
+          handleChange={handleChangeObjectState}
+          inputValueState={inputValueState}
+          buttonName="登録"
+          buttonState={buttonState}
+        />
+        <Spacer />
+        <SecondaryButton
+          buttonName="ログインはこちら"
+          type="button"
+          buttonState={buttonState}
+          onClick={() => setMode('SignIn')}
+        />
+        <SecondaryButton
+          buttonName="パスワードを忘れた方はこちら"
+          type="button"
+          buttonState={buttonState}
+          onClick={() => setMode('ResetPassword')}
+        />
+      </Card>
+    </Stack>
   );
 });
 

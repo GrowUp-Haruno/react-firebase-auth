5ad0b35f6c8878741efca7b9623039a822fe134d
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-16 15:08:38 +0900

    ヘッダー側に実装

--- src/atoms/HeaderNavi.tsx
+++ src/atoms/HeaderNavi.tsx
@@ -1,21 +1,22 @@
 import { Box, HStack, Spacer } from '@chakra-ui/react';
-import {FC} from 'react'
+import { FC } from 'react';
+import UserMenu from '../components/UserMenu';
 
 //Propsの型定義
 //type PropsType = {
-//	
+//
 //}
 
-const HeaderNavi:FC = (props) => {
+const HeaderNavi: FC = (props) => {
   return (
-    <HStack h="5vh">
+    <HStack h="5vh" spacing="4">
       <Box p="4">Logo</Box>
       <Box p="4">Logo</Box>
       <Spacer />
-      <Box p="4">Logo</Box>
+      <UserMenu />
     </HStack>
-  ); 
-}
+  );
+};
 
-HeaderNavi.displayName = 'HeaderNavi'
-export default HeaderNavi
+HeaderNavi.displayName = 'HeaderNavi';
+export default HeaderNavi;

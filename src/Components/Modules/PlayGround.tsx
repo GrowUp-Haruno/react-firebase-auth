7ce773b0e6101fb46835722b1e5b9b024e429025
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-21 23:26:46 +0900

    不要な部分を削除

--- src/Components/Modules/PlayGround.tsx
+++ src/Components/Modules/PlayGround.tsx
@@ -9,58 +9,29 @@ import 'react-image-crop/dist/ReactCrop.css';
 
 import { ChangeEventHandler, FC, useState } from 'react';
 import Card from '../Elements/Card';
+import SendButton from '../Elements/SendButton';
 
 //Propsの型定義
 type PropType = {};
 
-const PlayGround: FC<PropType> = () => {
-  const playGroundWidth = 1000;
-  const playGroundPadding = 8;
-  const playGroundClientWidth = 1000 - 8 * 4 * 2;
+const playGroundWidth = 1000;
+const playGroundPadding = 8;
+const cropSize = 96;
 
+const PlayGround: FC<PropType> = () => {
   const [imgSrc, setImgSrc] = useState<string>('');
   const [image, setImage] = useState<HTMLImageElement>();
   const [cropImage, setCropImage] = useState<string>('');
+  const [cropBlob, setCropBlob] = useState<Blob>();
+  const [crop, setCrop] = useState<ReactCrop.Crop>({ aspect: 1 });
 
   const reader = new FileReader();
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     const file = event.target.files![0];
-    // console.log(file)
-
-    const img = document.createElement('img');
-    reader.onload = (ev: any) => {
-      img.src = ev.target.result;
-      const aspectRaito = img.width / img.height;
-      const canvas = document.createElement('canvas');
-      if (
-        true
-        // crop.width !== undefined &&
-        // crop.height !== undefined &&
-        // crop.x !== undefined &&
-        // crop.y !== undefined
-      ) {
-        canvas.width = playGroundClientWidth;
-        canvas.height = playGroundClientWidth / aspectRaito;
-        console.log(`canvasWidth:${canvas.width}, canvasHeight:${canvas.height}`);
-        
-        const ctx = canvas.getContext('2d')
-        ctx?.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height)
-        canvas.toBlob((blob) => {
-          console.log(file.size)
-          console.log(blob!.size)
-        }, 'image/jpeg', 1)
-        setImgSrc(canvas.toDataURL('image/webp', 1));
-      }
-      // setImgSrc(ev.target.result);
-      // console.log(ev.target.result);
-    };
+    reader.onload = (ev: any) => setImgSrc(ev.target.result);
     reader.readAsDataURL(file);
   };
-
-  const [crop, setCrop] = useState<ReactCrop.Crop>({
-    aspect: 1,
-  });
 
   const getCroppedImg = async () => {
     if (
@@ -71,32 +42,18 @@ const PlayGround: FC<PropType> = () => {
       crop.y !== undefined
     ) {
       try {
-        // const canvas = document.createElement('canvas');
         const canvas = document.createElement('canvas');
         const scaleX = image.naturalWidth / image.width;
         const scaleY = image.naturalHeight / image.height;
-        // console.log(`naturalWidth:${image.naturalWidth}`);
-        // console.log(`naturalHeight:${image.naturalHeight}`);
-        // console.log(`Width:${image.width}`);
-        // console.log(`Height:${image.height}`);
-        // console.log(`scaleX:${image.naturalWidth / image.width}`);
-        // console.log(`scaleY:${image.naturalHeight / image.height}`);
 
         // 切り取り範囲の幅と高さをcanvasにセット
-
-        canvas.width = 192;
-        canvas.height = 192;
-
-        // canvas.width = crop.width;
-        // canvas.height = crop.height;
-        // console.log(`canvas.width:${canvas.width}`);
-        // console.log(`canvas.height:${canvas.height}`);
-        // console.log(`crop.x:${crop.x}`);
-        // console.log(`crop.y:${crop.y}`);
+        canvas.width = cropSize;
+        canvas.height = cropSize;
 
         // canvaに描画するためのコンテキストを取得してctxへセット
         const ctx = canvas.getContext('2d');
 
+        // canvasに切り取った画像を描画
         ctx?.drawImage(
           image,
           crop.x * scaleX,
@@ -105,34 +62,24 @@ const PlayGround: FC<PropType> = () => {
           crop.height * scaleY,
           0,
           0,
-          192,
-          192
-
-          // crop.width,
-          // crop.height
+          canvas.width,
+          canvas.height
         );
 
-        console.log(canvas.width);
+        // canvasからbase64(DataURI)へ変換
+        const base64Image = canvas.toDataURL('image/jpeg', 0.6);
+        setCropImage(base64Image);
 
-        const base64Image = canvas.toDataURL('image/png', 1);
-        // console.log(canvas);
-
-        // reader.onload = (ev: any) => {
-        //   console.log(ev.target.result);
-        // };
-
+        // canvasからBlobへ変換
         canvas.toBlob(
           (blob) => {
             if (blob) {
-              console.log(blob);
-              // reader.readAsDataURL(blob);
+              setCropBlob(blob);
             }
           },
-          'image/png',
-          1
+          'image/jpeg',
+          0.6
         );
-
-        setCropImage(base64Image);
       } catch (e) {
         console.log('crop the image');
       }
@@ -174,6 +121,7 @@ const PlayGround: FC<PropType> = () => {
           }}
           onDragEnd={getCroppedImg}
         />
+        <SendButton buttonName={'送信'} buttonState={false} />
       </Stack>
       <VStack>
         <Card>

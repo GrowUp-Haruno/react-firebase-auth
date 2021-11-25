a862bfbc0f736826f9b6ded7adbcf99e25b43237
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-25 11:22:01 +0900

    playgroundを初期化

--- src/Components/Modules/PlayGround.tsx
+++ src/Components/Modules/PlayGround.tsx
@@ -1,18 +1,10 @@
-import { Avatar } from '@chakra-ui/avatar';
-import { Button } from '@chakra-ui/button';
-import { Image, Img } from '@chakra-ui/image';
-import { Input } from '@chakra-ui/input';
-import { HStack, Stack, VStack } from '@chakra-ui/layout';
+import { HStack, Stack } from '@chakra-ui/layout';
 
-import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
 
 import { FC } from 'react';
-import Card from '../Elements/Card';
-import SendButton from '../Elements/SendButton';
 import { usePlayGround } from './hooks/usePlayGround';
 import { Heading } from '@chakra-ui/react';
-
 
 //Propsの型定義
 type PropType = {};
@@ -21,66 +13,18 @@ const playGroundWidth = 1000;
 const playGroundPadding = 8;
 
 const PlayGround: FC<PropType> = () => {
-  const {
-    crop,
-    imgSrc,
-    cropImage,
-    downloadUrl,
-    setImage,
-    handleSetImage,
-    getCroppedImg,
-    handleReactCrop,
-    handleUploadFromBlob,
-    handleGetDownloadURL,
-  } = usePlayGround();
-  console.log(downloadUrl)
+
   return (
     <HStack spacing={10}>
       <Stack
-        h={'195vh'}
+        h={'192vh'}
         w={playGroundWidth}
         p={playGroundPadding}
         backgroundColor={'gray.50'}
         spacing={10}
       >
-        <Heading size="sm">Play Ground</Heading>
-        {/* <Heading fontSize="3xl">PlayGround</Heading> */}
-        {/* <Button
-          as="label"
-          backgroundColor={'blue.300'}
-          color={'gray.100'}
-          type="submit"
-          _hover={{ backgroundColor: 'blue.500' }}
-          _loading={{ backgroundColor: 'green.500' }}
-        >
-          画像を選択
-          <Input
-            type="file"
-            display="none"
-            onChange={handleSetImage}
-            accept="image/png,image/jpeg"
-          />
-        </Button>
-        <ReactCrop
-          src={imgSrc}
-          crop={crop}
-          onChange={handleReactCrop}
-          circularCrop={true}
-          onImageLoaded={(image) => {
-            setImage(image);
-          }}
-          onDragEnd={getCroppedImg}
-        />
-        <SendButton buttonName={'送信'} buttonState={false} handleClick={handleUploadFromBlob} />
-        <SendButton buttonName={'取得'} buttonState={false} handleClick={handleGetDownloadURL} />
-        <Img src={downloadUrl} /> */}
+        <Heading fontSize="3xl">PlayGround</Heading>
       </Stack>
-      {/* <VStack>
-        <Card>
-          <Avatar src={cropImage} size="md" />
-          <Image src={cropImage} size="md" w={'48px'} id="img" />
-        </Card>
-      </VStack>  */}
     </HStack>
   );
 };

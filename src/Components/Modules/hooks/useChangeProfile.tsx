1ee8d40233d4b8b588ed20147cb873f5a58f1229
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-30 13:19:56 +0900

    非正規化に伴い、usersへの登録を廃止

--- src/Components/Modules/hooks/useChangeProfile.tsx
+++ src/Components/Modules/hooks/useChangeProfile.tsx
@@ -1,10 +1,10 @@
 import { useForm } from './useForm';
 import { ChangeUserProfileTypes } from '../types/typeChangeUserProfile';
 import { useFirebase } from './useFirebase';
-import { auth, avatarStorageUrl, database, storage } from '../../../firebase';
+import { auth, avatarStorageUrl, storage } from '../../../firebase';
 import { ChangeEventHandler, FormEventHandler, useCallback, useMemo, useState } from 'react';
 import { getDownloadURL, ref, UploadMetadata, uploadString } from 'firebase/storage';
-import { ref as dbRef, set } from 'firebase/database';
+// import { ref as dbRef, set } from 'firebase/database';
 import { useToast } from '@chakra-ui/react';
 
 export const useChangeProfile = () => {
@@ -85,7 +85,6 @@ export const useChangeProfile = () => {
   // BlobからFirebase Storageへアップロード
   const { changeUserProfile } = useFirebase();
 
-
   const handleUploadFromBlob = useCallback<FormEventHandler<HTMLDivElement>>(
     async (event) => {
       setButtonState(true);
@@ -95,7 +94,7 @@ export const useChangeProfile = () => {
         cacheControl: 'public,max-age=3600,immutable',
       };
 
-      const dbUsersRef = dbRef(database, `users/${auth.currentUser!.uid}`);
+      // const dbUsersRef = dbRef(database, `users/${auth.currentUser!.uid}`);
       try {
         if (crop.width) {
           await uploadString(storageRef, cropImage, 'data_url', metadata);
@@ -111,20 +110,20 @@ export const useChangeProfile = () => {
             photoUrl: result ? result : '',
           });
 
-          await set(dbUsersRef, {
-            userName: inputValueState.userName,
-            photoUrl: result ? result : '',
-          });
+          // await set(dbUsersRef, {
+          //   userName: inputValueState.userName,
+          //   photoUrl: result ? result : '',
+          // });
         } else {
           await changeUserProfile({
             userName: inputValueState.userName,
             photoUrl: inputValueState.photoUrl,
           });
 
-          await set(dbUsersRef, {
-            userName: inputValueState.userName,
-            photoUrl: inputValueState.photoUrl,
-          });
+          // await set(dbUsersRef, {
+          //   userName: inputValueState.userName,
+          //   photoUrl: inputValueState.photoUrl,
+          // });
         }
         setButtonState(false);
         toast({

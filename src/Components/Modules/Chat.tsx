56415a8f3923b7dd49f2bf06c3f8e12aa44811a6
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-11-30 23:34:56 +0900

    srcを固定値から引数方式に修正

--- src/Components/Modules/Chat.tsx
+++ src/Components/Modules/Chat.tsx
@@ -1,33 +1,41 @@
 import { Button } from '@chakra-ui/button';
 import { Input } from '@chakra-ui/input';
 import { Box, Heading, HStack, Stack } from '@chakra-ui/layout';
-import { FC } from 'react';
+import { FC, memo } from 'react';
 import AvatarBox from '../Elements/AvatarBox';
 import { auth } from '../../firebase';
+import { User } from 'firebase/auth';
 
 //Propsの型定義
-// type PropsType = {
+type PropType = {
+  signInUser: User;
+};
 
-// }
-
-const Chat: FC = () => {
+const Chat: FC<PropType> = memo(({ signInUser }) => {
   return (
     <Stack h={'95vh'} w={1000} p={8} backgroundColor={'gray.50'}>
-      <AvatarBox>
+      <AvatarBox
+        uid={signInUser.uid}
+        // displayName={signInUser.displayName}   
+        photoURL={signInUser.photoURL}
+        key={signInUser.uid}
+      >
         <Heading size="sm">{auth.currentUser?.displayName?.toString()}</Heading>
-        <HStack justify='flex-end'>
-        <Input variant="flushed" placeholder="今どうしてる？" />
+        <HStack justify="flex-end">
+          <Input variant="flushed" placeholder="今どうしてる？" />
 
-        <Button borderRadius='25' colorScheme="blue">送信</Button>
+          <Button borderRadius="25" colorScheme="blue">
+            送信
+          </Button>
         </HStack>
       </AvatarBox>
-      <AvatarBox>
+      {/* <AvatarBox signInUser={signInUser}>
         <Heading size="sm">ユーザー</Heading>
         <Box>あのイーハトーヴォのすきとほった風、夏でも底に冷たさをもつ青いそら</Box>
-      </AvatarBox>
+      </AvatarBox> */}
     </Stack>
   );
-};
+});
 
 Chat.displayName = 'Chat';
 export default Chat;

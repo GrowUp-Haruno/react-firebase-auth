81cb3405d83b66aa860e56c8429d5e6a6deadd31
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-12-02 10:15:35 +0900

    チャットが空の状態(snapshotValがnull)だとエラーが出てしまう問題を解消

--- src/Components/Modules/ChatView.tsx
+++ src/Components/Modules/ChatView.tsx
@@ -4,6 +4,7 @@ import { Box, Heading } from '@chakra-ui/react';
 import AvatarBox from '../Elements/AvatarBox';
 import { ChatViewPropType } from './types/typeChat';
 import { useChatView } from './hooks/useChat';
+import Loading from './Loading';
 
 /**
  * チャットの入出力を表示
@@ -11,23 +12,28 @@ import { useChatView } from './hooks/useChat';
  * @argument { category　} category - チャットのカテゴリ
  */
 const ChatView: FC<ChatViewPropType> = memo(({ category }) => {
-  const { snapshotVal } = useChatView(category);
-  
+  const { snapshotVal, loading } = useChatView(category);
+
   return (
     <>
-      {Object.values(snapshotVal)
-        .reverse()
-        .map((value, index) => (
-          <AvatarBox
-            uid={value.uid}
-            displayName={value.displayName}
-            photoURL={value.photoURL}
-            key={`${value.uid}-${index}`}
-          >
-            <Heading size="sm">{value.displayName}</Heading>
-            <Box>{value.tweet}</Box>
-          </AvatarBox>
-        ))}
+      {loading ? (
+        <Loading />
+      ) : (
+        snapshotVal &&
+        Object.values(snapshotVal)
+          .reverse()
+          .map((value, index) => (
+            <AvatarBox
+              uid={value.uid}
+              displayName={value.displayName}
+              photoURL={value.photoURL}
+              key={`${value.uid}-${index}`}
+            >
+              <Heading size="sm">{value.displayName}</Heading>
+              <Box>{value.tweet}</Box>
+            </AvatarBox>
+          ))
+      )}
     </>
   );
 });
